1a0c98ee34fc7835359ab2a4c21e8544c3fa3c68
Rafael Ponte <rafael.ponte@zup.com.br>
2022-10-03 17:34:25 -0300

    isolation level implementation + retryable

--- src/main/java/br/com/stackspot/nullbank/Application.java
+++ src/main/java/br/com/stackspot/nullbank/Application.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class) // Needed by Zalando Problem lib
 public class Application {
---
+++ src/main/java/br/com/stackspot/nullbank/withdrawal/RepeatableReadIsolationLevelATMService.java
@@ -0,0 +1,43 @@
+package br.com.stackspot.nullbank.withdrawal;
+
+import org.hibernate.exception.LockAcquisitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class RepeatableReadIsolationLevelATMService {
+
+    @Autowired
+    private AccountRepository repository;
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Retryable(
+            value = LockAcquisitionException.class,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 100, random = true, multiplier = 2.0)
+    )
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public void withdraw(Long accountId, double amount) {
+
+        Account account = repository.findById(accountId).orElseThrow(() -> {
+            throw new IllegalStateException("account does not exist: " + accountId);
+        });
+
+        double newBalance = (account.getBalance() - amount);
+        if (newBalance < 0) {
+            throw new IllegalStateException("there's not enough balance");
+        }
+
+        account.setBalance(newBalance);
+        repository.save(account);
+
+        transactionRepository
+                .save(new Transaction(account, amount, "withdraw"));
+    }
+
+}
